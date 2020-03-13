7249acfbd3b3dafc408076cdf37ef8e7febc5347
Albert Diaz <l2claims@yahoo.com>
2020-03-13 12:17:17 -0400

    Added If/Else statement to initializer session_store to set cookie to domain if hosted or local if in dev productiob

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
-Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain:'http://authentication-my-app-for-production.herokuapp.com' 
-#MAIN NAMING CONVENTION.
+if  Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app', domain:'http://authentication-my-app-for-production.herokuapp.com'
+else 
+    Rails.application.config.session_store :cookie_store, key: '_authentication_app'
+end
 
+#MAIN NAMING CONVENTION.
 #DOMAIN = STRING AND WHATEVER
 #THE DOMAIN NAME IS WHERE THE APP WILL BE HOSTED.
